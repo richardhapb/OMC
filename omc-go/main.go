300e619eff71f55e7f90a3f1502ca7d7034f8523
richardhapb <richard.hapb@icloud.com>
2025-04-20 22:22:20 -0400

    feat: add WebSocket and message response handling
    - Introduced WebSocket handling with proper handshake response.
    - Added `/messages` endpoint to serve messages as JSON.
    - Refactored message storage and mutex usage for better readability.
    - Fixed formatting issue in Content-Length header construction.
    - Improved HTTP response construction for compliance with standards.

--- omc-go/main.go
+++ omc-go/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"fmt"
 	"log"
 	"net"
 	"slices"
+	"strings"
 	"sync"
 	"time"
 )
@@ -13,10 +17,15 @@ type Message struct {
 	timestamp int64
 }
 
-var messages []Message
-var mutex sync.Mutex
+var (
+	messages []Message
+	mutex	 sync.Mutex
+)
 
-const MessageDisappearThreshold = 60000 // One minute
+const (
+	MessageDisappearThreshold = 60000 // One minute
+	MagicGUID				  = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+)
 
 func main() {
 	connStr := "127.0.0.1:8000"
@@ -60,12 +69,103 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		mutex.Lock()
-		messages = append(messages, Message{text: string(buffer[:n]), timestamp: time.Now().UnixMilli()})
-		mutex.Unlock()
+		// mutex.Lock()
+		// messages = append(messages, Message{text: string(buffer[:n]), timestamp: time.Now().UnixMilli()})
+		// mutex.Unlock()
+
+		handleRequest(buffer, n, &conn)
 
 		log.Printf("Received: %s\n", buffer[:n])
 	}
+}
+
+func handleRequest(buffer []byte, n int, conn *net.Conn) {
+	request := string(buffer[:n])
+	lines := strings.Split(request, "\n")
+
+	parts := strings.Split(lines[0], " ")
+	method := parts[0]
+	uri := parts[1]
+	if method == "GET" && uri == "/ws" {
+		go handleWebSocket(lines, conn)
+	}
+
+	if method == "GET" && uri == "/messages" {
+		go responseMessages(conn)
+	}
+}
+
+func handleWebSocket(lines []string, conn *net.Conn) {
+	var key string
+	for _, line := range lines {
+		if strings.HasPrefix(line, "Sec-WebSocket-Key:") {
+			parts := strings.SplitN(line, ":", 2)
+			key = strings.TrimSpace(parts[1])
+			break
+		}
+
+	}
+
+	if key == "" {
+		log.Println("No WebSocket key found")
+		return
+	}
+
+	acceptKey := calculateAcceptKey(key)
+
+	response := "HTTP/1.1 101 Switching Protocols\r\n" +
+		"Upgrade: websocket\r\n" +
+		"Connection: Upgrade\r\n" +
+		"Sec-WebSocket-Accept: " + acceptKey + "\r\n\r\n"
+
+	log.Println()
+	log.Printf("Response to WS connection:\n%s\n", response)
+	log.Println()
+
+	_, err := (*conn).Write([]byte(response))
+
+	if err != nil {
+		log.Printf("Error connecting WebSocket: %s\n", err)
+	}
+}
+
+func responseMessages(conn *net.Conn) {
+
+	header := "HTTP/1.1 200 OK\r\n"
+	header += "Content-Type: application/json\r\n"
+	header += "access-control-allow-origin: *\r\n"
+
+	body := "{\"messages\":["
+
+	for _, message := range messages {
+		body += fmt.Sprintf("{\"message\": \"%s\", \"timestamp\": %d},", message.text, message.timestamp)
+	}
+
+	header += fmt.Sprintf("Content-Length: %d", calculateContentLength(body))
+	header += "\r\n\r\n"
+
+	if strings.HasSuffix(body, ",") {
+		body = body[:len(body)-1]
+	}
+
+	body += "]}"
+
+	response := header + body + "\r\n\r\n"
+
+	log.Printf("Response to messages: \n%s\n", response)
+
+	(*conn).Write([]byte(response))
+}
+
+func calculateContentLength(body string) int {
+	return len(body) + 2
+}
+
+func calculateAcceptKey(key string) string {
+	h := sha1.New()
+	h.Write([]byte(key + MagicGUID))
+
+	return base64.StdEncoding.EncodeToString(h.Sum(nil))
 }
 
 func removeOldMessages(messages *[]Message) {
@@ -75,7 +175,7 @@ func removeOldMessages(messages *[]Message) {
 			now := time.Now().UnixMilli()
 			if now-(*messages)[i].timestamp >= MessageDisappearThreshold {
 				log.Printf("Removing message... %s\n", (*messages)[i].text)
-				*messages = slices.Delete(*messages, i, i + 1)
+				*messages = slices.Delete(*messages, i, i+1)
 			}
 
 		}
